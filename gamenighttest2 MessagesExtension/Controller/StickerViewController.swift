cbdc18b56ca51c77c7a63734bf68a2966b60aeb2
DRSM <daimenambers@yahoo.com>
2023-11-08 13:01:21 -0500

    Rearranged the StickerViewController and removed the marijuana sticker. Updated the version to build 2.

--- gamenighttest2 MessagesExtension/Controller/StickerViewController.swift
+++ gamenighttest2 MessagesExtension/Controller/StickerViewController.swift
@@ -11,33 +11,30 @@ import Messages
 class StickerViewController: MSStickerBrowserViewController {
     static let storyboardIdentifier = "StickerViewController"
     var stickers = [MSSticker]()
-    weak var delegate: MessageDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Stickers"
+        createSticker(asset: "GameNight Logo", localizedDescription: "Game Night Logo")
+        createSticker(asset: "It's Gamenight", localizedDescription: "It's Gamenight")
+        createSticker(asset: "It's Intermission", localizedDescription: "It's Intermission")
         createSticker(asset: "1stPlace", localizedDescription: "1st Place")
         createSticker(asset: "2ndPlace", localizedDescription: "2nd Place")
         createSticker(asset: "3rdPlace", localizedDescription: "3rd Place")
-        createSticker(asset: "Bad Dinner", localizedDescription: "Bad Dinner")
-        createSticker(asset: "Blue Team", localizedDescription: "Blue Team")
-        createSticker(asset: "Did you tell them?", localizedDescription: "Did you tell them?")
-        createSticker(asset: "GameNight Logo", localizedDescription: "Game Night Logo")
-        createSticker(asset: "Get Outta Here", localizedDescription: "Get Outta Here")
-        createSticker(asset: "Good Dinner", localizedDescription: "Good Dinner")
-        createSticker(asset: "I'm so high rn", localizedDescription: "I'm so high rn")
-        createSticker(asset: "idk timezones", localizedDescription: "I don't know timezones")
-        createSticker(asset: "It's Gamenight", localizedDescription: "It's Gamenight")
-        createSticker(asset: "It's Intermission", localizedDescription: "It's Intermission")
-        createSticker(asset: "‎Let's Coast Logo", localizedDescription: "Let's Coast Logo")
-        createSticker(asset: "Nice Try", localizedDescription: "Nice Try")
-        createSticker(asset: "Plus Six", localizedDescription: "Plus Six")
-        createSticker(asset: "Red Team", localizedDescription: "Red Team")
-        createSticker(asset: "Too Bad", localizedDescription: "Too Bad")
-        createSticker(asset: "Youre Welcome", localizedDescription: "You're Welcome")
         createSticker(asset: "YouTried", localizedDescription: "You Tried")
-
+        createSticker(asset: "Did you tell them?", localizedDescription: "Did you tell them?")
+        createSticker(asset: "Good Dinner", localizedDescription: "Good Dinner")
+        createSticker(asset: "Bad Dinner", localizedDescription: "Bad Dinner")
+        createSticker(asset: "Red Team", localizedDescription: "Red Team")
+        createSticker(asset: "Blue Team", localizedDescription: "Blue Team")
+        createSticker(asset: "idk timezones", localizedDescription: "I don't know timezones")
+        createSticker(asset: "Get Outta Here", localizedDescription: "Get Outta Here")
+        createSticker(asset: "Too Bad", localizedDescription: "Too Bad")
+        createSticker(asset: "Nice Try", localizedDescription: "Nice Try")
+        createSticker(asset: "Youre Welcome", localizedDescription: "You're Welcome")
+        createSticker(asset: "Plus Six", localizedDescription: "Plus Six")
+        createSticker(asset: "‎Let's Coast Logo", localizedDescription: "Let's Coast Logo")
     }
     
     /// Creates a MSSticker to use in the app
@@ -61,7 +58,7 @@ class StickerViewController: MSStickerBrowserViewController {
           // localizedDescription for accessibility
 
           stickers.append(sticker)
-        }catch {
+        } catch {
           print(error)
           return
         }
